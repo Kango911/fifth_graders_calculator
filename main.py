dfd7dc6154ad6736abe0c842b49c70aed605a618
Kango911 <lipenkov.a61@gmail.com>
2024-06-20 21:14:25 +0500

    ref code

--- main.py
+++ main.py
@@ -1,63 +1,11 @@
-class ArabicOrRomanian:
-    char_values = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
-    }
-
-    @staticmethod
-    def arabic_to_romanian(arabic_number):
-        if arabic_number > 5000:
-            print(
-                "Число превышает максимально допустимое значение, пожалуйста введите новое число меньше или равное 5000")
-            arabic_number = int(input())
-            return ArabicOrRomanian.arabic_to_romanian(arabic_number)
-        else:
-            thousands = ["", "M", "MM", "MMM", "MMMM", "MMMMM"]
-            hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-            tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-            ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-
-            result = ""
-            result += thousands[arabic_number // 1000]
-            arabic_number %= 1000
-
-            result += hundreds[arabic_number // 100]
-            arabic_number %= 100
-
-            result += tens[arabic_number // 10]
-            arabic_number %= 10
-
-            result += ones[arabic_number]
-            return result
-
-    @staticmethod
-    def romanian_to_arabic(roman):
-        if not roman:
-            return 0
-        roman = roman.upper()
-
-        if roman[0] == '(':
-            pos = roman.rindex(')')
-            part1 = roman[1:pos]
-            part2 = roman[pos + 1:]
-            return 1000 * ArabicOrRomanian.romanian_to_arabic(part1) + ArabicOrRomanian.romanian_to_arabic(part2)
-
-        total = 0
-        last_value = 0
-        for i in range(len(roman) - 1, -1, -1):
-            new_value = ArabicOrRomanian.char_values[roman[i]]
-            if new_value < last_value:
-                total -= new_value
-            else:
-                total += new_value
-                last_value = new_value
-        return total
-
+import sys
+import math
 
 class Menu:
     @staticmethod
     def display_menu():
-        print(f"Выполнил Lipenkov A.D. Группа: ПИ-101\n")
-        print(f"Это консольное приложение является калькулятором для систем счисления от 1 до 50 включительно, а также служит калькулятором для перевода чисел в римскую СС.\n\n")
+        print("     Это консольное приложение является калькулятором для систем счисления от 1 до 50 включительно, а также служит калькулятором для перевода чисел в римскую СС.")
+        print("     Выполнил Lipenkov A.D. Группа: ПрИ-101")
         print("Выберите что вы хотите сделать:")
         print("1. Перевести из одной системы счисления в другую.")
         print("2. Перевести число из Арабской в Римскую и наоборот.")
@@ -66,116 +14,43 @@ class Menu:
         print("5. Умножение в произвольной системе счисления")
         print("Все действия необходимо подтверждать нажатием клавиши Enter.")
         choice = input()
-        if choice == "1":
-            Menu.convert_between_bases()
-        elif choice == "2":
-            Menu.arabic_roman_conversion()
-        elif choice == "3":
-            Menu.perform_addition()
-        elif choice == "4":
-            Menu.perform_subtraction()
-        elif choice == "5":
-            Menu.perform_multiplication()
+        if choice == '1':
+            ConvertingToAnyBase.transfer_to_another_system()
+            Menu.return_to_menu()
+        elif choice == '2':
+            ArabicOrRomanian.choose_the_system()
+            Menu.return_to_menu()
+        elif choice == '3':
+            SummaryInAnySystem.summary()
+            Menu.return_to_menu()
+        elif choice == '4':
+            SubtractionInAnySystem.subtraction()
+            Menu.return_to_menu()
+        elif choice == '5':
+            MultiplicationInAnySystem.multiplication()
+            Menu.return_to_menu()
 
     @staticmethod
-    def convert_between_bases():
-        print("Введите число:")
-        number = input()
-        print("Введите систему счисления:")
-        system = int(input())
-
-        verification = int(number)
-        while verification > 0:
-            if verification % 10 >= system:
-                print(
-                    "Вы ввели неправильное число. Число не должно содержать цифр больше или равных значению системы счисления.")
-                return Menu.convert_between_bases()
-            verification //= 10
-
-        num_in_base = Converter.convert_to_dec(number, system)
-        print(f"Введите в какую систему счисления осуществить перевод:")
-        target_base = int(input())
-        converted = Converter.convert_from_dec(num_in_base, target_base)
-        print(f"Ответ в {target_base}-ой системе: {converted}")
-
-    @staticmethod
-    def arabic_roman_conversion():
-        print("Нажмите 1, если хотите перевести арабские цифры в римские")
-        print("Нажмите 2, если хотите перевести римские цифры в арабские")
-        choice = int(input())
-        if choice == 1:
-            print("Введите число, которое хотите перевести")
-            arabic = int(input())
-            print(ArabicOrRomanian.arabic_to_romanian(arabic))
-        elif choice == 2:
-            print("Введите римское значение числа:")
-            roman = input()
-            print(ArabicOrRomanian.romanian_to_arabic(roman))
+    def return_to_menu():
+        print("Хотите ли вы еще воспользоваться программой?")
+        print("Если хотите то введите 1.")
+        print("Иначе введите что угодно.")
+        choice = input()
+        if choice == '1':
+            print("", end='')
+            Menu.display_menu()
         else:
-            print("Вы ввели неправильное значение.")
-            return Menu.arabic_roman_conversion()
+            sys.exit()
+
+class ConvertingToAnyBase:
+    numbers = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 19: 'J', 21: 'K', 22: 'L',
+               23: 'M', 24: 'N', 25: 'O', 26: 'P', 27: 'Q', 28: 'R', 29: 'S', 30: 'T', 31: 'U', 32: 'V', 33: 'W', 34: 'X',
+               35: 'Y', 36: 'Z', 37: 'a', 38: 'b', 39: 'c', 40: 'd', 41: 'e', 42: 'f', 43: 'g', 44: 'h', 45: 'i', 46: 'j',
+               47: 'k', 48: 'l', 49: 'm', 50: 'n'}
 
     @staticmethod
-    def perform_addition():
-        print("Введите первое число:")
-        number1 = input()
-        print("Введите второе число:")
-        number2 = input()
-        print("Введите систему счисления:")
-        system = int(input())
-
-        num1_dec = Converter.convert_to_dec(number1, system)
-        num2_dec = Converter.convert_to_dec(number2, system)
-        result_dec = num1_dec + num2_dec
-        result_base = Converter.convert_from_dec(result_dec, system)
-        print(f"Их сумма в {system}-ой системе: {result_base}")
-
-    @staticmethod
-    def perform_subtraction():
-        print("Введите первое число:")
-        number1 = input()
-        print("Введите второе число:")
-        number2 = input()
-        print("Введите систему счисления:")
-        system = int(input())
-
-        num1_dec = Converter.convert_to_dec(number1, system)
-        num2_dec = Converter.convert_to_dec(number2, system)
-        if num1_dec < num2_dec:
-            print("Первое число должно быть больше второго!")
-            return Menu.perform_subtraction()
-
-        result_dec = num1_dec - num2_dec
-        result_base = Converter.convert_from_dec(result_dec, system)
-        print(f"Их разность в {system}-ой системе: {result_base}")
-
-    @staticmethod
-    def perform_multiplication():
-        print("Введите первое число:")
-        number1 = input()
-        print("Введите второе число:")
-        number2 = input()
-        print("Введите систему счисления:")
-        system = int(input())
-
-        num1_dec = Converter.convert_to_dec(number1, system)
-        num2_dec = Converter.convert_to_dec(number2, system)
-        result_dec = num1_dec * num2_dec
-        result_base = Converter.convert_from_dec(result_dec, system)
-        print(f"Их произведение в {system}-ой системе: {result_base}")
-
-
-class Converter:
-    numbers = {
-        10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I", 19: "J", 20: "K", 21: "L",
-        22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R", 28: "S", 29: "T", 30: "U", 31: "V", 32: "W", 33: "X",
-        34: "Y", 35: "Z", 36: "a", 37: "b", 38: "c", 39: "d", 40: "e", 41: "f", 42: "g", 43: "h", 44: "i", 45: "j",
-        46: "k", 47: "l", 48: "m", 49: "n"
-    }
-
-    @staticmethod
-    def convert_from_dec(num, sys):
-        result = ""
+    def convert_from_dec(num, base):
+        result = ''
         pos = num
         while True:
             if pos == 1:
@@ -183,23 +58,223 @@ class Converter:
                 break
             elif pos == 0:
                 break
-            ost = pos % sys
-            if ost > 9:
-                result += Converter.numbers[ost]
+            remainder = pos % base
+            if remainder > 9:
+                result += ConvertingToAnyBase.numbers[remainder]
             else:
-                result += str(ost)
-            pos //= sys
-        return result[::-1]
+                result += str(remainder)
+            pos //= base
+        return ConvertingToAnyBase.reverse(result)
 
     @staticmethod
-    def convert_to_dec(num, sys):
+    def convert_to_dec(num, base):
         result = 0
         s = 0
         for i in range(len(num) - 1, -1, -1):
-            result += int(num[i]) * (sys ** s)
+            result += int(math.pow(base, s)) * int(num[i])
             s += 1
         return result
 
+    @staticmethod
+    def transfer_to_another_system():
+        print("", end='')
+        print("Введите число:")
+        number = input()
+        print("", end='')
+        print("Введите систему счисления:")
+        system = int(input())
+        if int(number) <= 0 or system <= 0:
+            print("", end='')
+            print("Введите число больше нуля!")
+            print("Для продолжения нажмите любую кнопку.")
+            input()
+            ConvertingToAnyBase.transfer_to_another_system()
+        else:
+            verification = int(number)
+            while verification > 0:
+                check = verification % 10
+                if check >= system:
+                    print("", end='')
+                    print("Вы ввели неправильное число. Число не должно содержать цифр больше или равных значению системы счисления.")
+                    print("Для продолжения нажмите любую кнопку.")
+                    input()
+                    ConvertingToAnyBase.transfer_to_another_system()
+                verification //= 10
+        print("", end='')
+        tru_num = list(number)
+        print(f"Берем последний элемент числа(в данном случае {tru_num[-1]})")
+        print(f"И умножаем его ({tru_num[-1]}) на систему счисления (в данном случае {system}) в степени начиная с 0")
+        ans = int(tru_num[-1]) * int(math.pow(system, 0))
+        print(f"В данном случае ответ будет {ans}")
+        print("Дальше берем следующий элемент и домножаем на систему счисления в степени на 1 больше.")
+        print(f"В конце складываем полученные результаты и получаем ответ(в десятичной системе):{ConvertingToAnyBase.convert_to_dec(tru_num, system)}")
+        print("Для продолжения нажмите любую кнопку.")
+        input()
+        print("", end='')
+        print("Введите в какую систему счисления осуществить перевод:")
+        sys2 = int(input())
+        if sys2 <= 0:
+            print("", end='')
+            print("Введите число больше нуля!")
+            print("Для продолжения нажмите любую кнопку.")
+            input()
+            ConvertingToAnyBase.transfer_to_another_system()
+        else:
+            print("", end='')
+            result = ConvertingToAnyBase.convert_from_dec(ConvertingToAnyBase.convert_to_dec(tru_num, system), sys2)
+            print(f"Ответ в {sys2}-ой системе: {result}")
+
+    @staticmethod
+    def reverse(line):
+        return line[::-1]
+
+class ArabicOrRomanian:
+    @staticmethod
+    def choose_the_system():
+        print("", end='')
+        print("Нажмите 1, если хотите перевести арабские цифры в римские")
+        print("Нажмите 2, если хотите перевести римские цифры в арабские")
+        choice = int(input())
+        if choice == 1:
+            print(ArabicOrRomanian.arabic_to_roman(ArabicOrRomanian.arabic_input()))
+        elif choice == 2:
+            print(ArabicOrRomanian.roman_to_arabic(ArabicOrRomanian.roman_input()))
+        else:
+            print("", end='')
+            print("Вы ввели неправильное значение.")
+            print("Для продолжения нажмите любую клавишу.")
+            input()
+            ArabicOrRomanian.choose_the_system()
+
+    @staticmethod
+    def arabic_input():
+        print("Введите число, которое хотите перевести")
+        arabic = int(input())
+        print("", end='')
+        return arabic
+
+    @staticmethod
+    def arabic_to_roman(arabic_number):
+        if arabic_number > 5000:
+            print("Число превышает максимально допустимое значение, пожалуйста введите новое число меньше или равное 5000")
+            arabic_number = int(input())
+            print("", end='')
+            return ArabicOrRomanian.arabic_to_roman(arabic_number)
+        else:
+            thousands = ["", "M", "MM", "MMM", "MMMM", "MMMMM"]
+            hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+            tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+            ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+            result = ''
+            result += thousands[arabic_number // 1000]
+            arabic_number %= 1000
+            result += hundreds[arabic_number // 100]
+            arabic_number %= 100
+            result += tens[arabic_number // 10]
+            arabic_number %= 10
+            result += ones[arabic_number]
+
+            return result
+
+    def roman_input():
+        print("Введите римское значение числа:")
+        roman = input()
+        print("", end='')
+        return roman
+
+    @staticmethod
+    def roman_to_arabic(roman):
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman = roman.upper()
+        total = 0
+        last_value = 0
+        for char in roman[::-1]:
+            value = roman_dict.get(char, 0)
+            if value >= last_value:
+                total += value
+            else:
+                total -= value
+            last_value = value
+        return total
+
+class SummaryInAnySystem:
+    @staticmethod
+    def summary():
+        print("", end='')
+        print("Введите первое число:")
+        number1 = input()
+        print("", end='')
+        print("Введите второе число:")
+        number2 = input()
+        print("", end='')
+        print("Введите систему счисления:")
+        system = int(input())
+        print("", end='')
+        if system <= 0:
+            print("Введите систему счисления больше нуля!")
+            print("Для продолжения нажмите любую кнопку.")
+            input()
+            SummaryInAnySystem.summary()
+        else:
+            num1 = ConvertingToAnyBase.convert_to_dec(number1, system)
+            num2 = ConvertingToAnyBase.convert_to_dec(number2, system)
+            result = num1 + num2
+            print(f"Их сумма в {system}-ой системе: {ConvertingToAnyBase.convert_from_dec(result, system)}")
+
+class SubtractionInAnySystem:
+    @staticmethod
+    def subtraction():
+        print("", end='')
+        print("Введите первое число:")
+        number1 = input()
+        print("", end='')
+        print("Введите второе число:")
+        number2 = input()
+        print("", end='')
+        print("Введите систему счисления:")
+        system = int(input())
+        print("", end='')
+        if system <= 0:
+            print("Введите систему счисления больше нуля!")
+            print("Для продолжения нажмите любую кнопку.")
+            input()
+            SubtractionInAnySystem.subtraction()
+        else:
+            num1 = ConvertingToAnyBase.convert_to_dec(number1, system)
+            num2 = ConvertingToAnyBase.convert_to_dec(number2, system)
+            if num1 < num2:
+                print("Первое число должно быть больше второго!")
+                print("Для продолжения нажмите любую кнопку.")
+                input()
+                SubtractionInAnySystem.subtraction()
+            else:
+                result = num1 - num2
+                print(f"Их разность в {system}-ой системе: {ConvertingToAnyBase.convert_from_dec(result, system)}")
+
+class MultiplicationInAnySystem:
+    @staticmethod
+    def multiplication():
+        print("", end='')
+        print("Введите первое число:")
+        number1 = input()
+        print("", end='')
+        print("Введите второе число:")
+        number2 = input()
+        print("", end='')
+        print("Введите систему счисления:")
+        system = int(input())
+        print("", end='')
+        if system <= 0:
+            print("Введите систему счисления больше нуля!")
+            print("Для продолжения нажмите любую кнопку.")
+            input()
+            MultiplicationInAnySystem.multiplication()
+        else:
+            num1 = ConvertingToAnyBase.convert_to_dec(number1, system)
+            num2 = ConvertingToAnyBase.convert_to_dec(number2, system)
+            result = num1 * num2
+            print(f"Их произведение в {system}-ой системе: {ConvertingToAnyBase.convert_from_dec(result, system)}")
 
 if __name__ == "__main__":
     Menu.display_menu()
